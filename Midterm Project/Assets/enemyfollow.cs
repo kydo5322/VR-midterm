0f59257fac286073d9e0d9f57f8fb91d1b77193a
Claire Sutton <blue26cutie@gmail.com>
2019-10-17 02:27:22 -0600

    Tracker Added!

---
+++ Midterm Project/Assets/enemyfollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyfollow : MonoBehaviour
+{
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 8;
+    int MinDist = 2;
+
+
+
+
+    void start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //Here Call any function U want Like Shoot at here or something
+            }
+
+        }
+    }
+}
+
+
