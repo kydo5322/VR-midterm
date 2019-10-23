d100d01eaedb7ca6589dd59f7982cd5af50fcea2
kydo5322 <kydo5322@colorado.edu>
2019-10-22 18:24:29 -0600

    raycast added (kinda)

---
+++ Midterm Project/Assets/RaycastToTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastToTarget : MonoBehaviour
+{
+
+    private int targetLayer = 1 << 8; // Layer 8 (targets)
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            // Parameters: position to start the ray, direction to project the ray, output for raycast, limit of ray length, and layer mask
+            if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, targetLayer))
+            {
+                //Debug.Log(hit.collider.gameObject.name);
+                Destroy(hit.collider.gameObject);
+            }
+
+        }
+    }
+}
