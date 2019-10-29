dd0208974b69cc5055f5587a3ccb6559da63c4ed
kydo5322 <kydo5322@colorado.edu>
2019-10-28 20:57:59 -0600

    spawn ghost

---
+++ Midterm Project/Assets/Respawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    //public Transform ghostDrop;
+    public GameObject enemy;
+    public int numToSpawn;
+    public Vector3 position;
+
+    void Awake()
+    {
+        Vector3 position = new Vector3(Random.Range(-5.0F, 5.0F), 1, Random.Range(-5.0F, 5.0F));
+    }
+
+    void Start()
+    {
+        int spawned = 0;
+
+        while (spawned < numToSpawn)
+        {
+            position = new Vector3(Random.Range(-5.0F, 5.0F), 1, Random.Range(-5.0F, 5.0F));
+            //Instantiate(ghostDrop, position, Quaternion.identity);
+            Instantiate(enemy, position, Quaternion.identity);
+            spawned++;
+        }
+    }
+}
